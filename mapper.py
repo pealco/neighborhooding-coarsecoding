2b15dbc152a4d4b7b7c2c1dc449ba88b1b4db9a6
Pedro Alcocer <pealco@gmail.com>
2009-04-13 14:22:39 -0400

    First commit.

---
+++ mapper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# A script for finding word neighborhood sizes.
+#
+# Author: Pedro Alcocer
+# Last modified: 12 Apr 2009
+# 
+# Usage: cat corpus.txt | mapper.py > output.txt
+#
+
+import sys
+
+def read_corpus(file):
+    """Reads the input file and creates a dictionary with pronunications as keys."""
+    
+    corpus = dict([("".join(line.split()[1:]), None) for line in file])
+    return corpus
+
+def edits(word):
+    """Finds permutations of _word_ with edit distance 1."""
+    
+    s = [(word[:i], word[i:]) for i in xrange(len(word))]
+    deletes    = [a + b[1:]               for a, b in s if b]
+    transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b) > 1]
+    replaces   = [a + c + b[1:]           for a, b in s for c in alphabet if b]
+    inserts    = [a + c + b               for a, b in s for c in alphabet]
+    return set(deletes + transposes + replaces + inserts)
+
+def known(words):
+    """Returns only words that are in the corpus."""
+    
+    return [word for word in words if word in corpus]
+    
+def main(sep="\t"):
+    for word in corpus:
+        print "%s%s%d" % (word, sep, len(known(edits(word))))
+
+if __name__ == "__main__":
+    
+    corpus   = read_corpus(sys.stdin)
+    alphabet = "a&@ObdDERfghIiklmnNprsStTUuvwjzZ"
+                
+    main()
---
+++ reducer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# A script for finding word PFNA hash cache sizes.
+#
+# Author: Pedro Alcocer
+# Last modified: 12 Apr 2009
+# 
+# Usage: cat corpus.txt | mapper.py | reducer.py > output.txt
+#
+
+from string import maketrans, translate
+from operator import itemgetter
+import sys
+
+def ipa2hash(ipa):
+    """Converts ASCII IPA to coarsecode."""
+    return clean(ipa.translate(CHAR_TO_CODE))
+
+def clean(input):
+    """Removes doubled letters from a string."""
+    output = input[0]
+    for char in input:
+        if output[-1] != char: 
+            output += char
+    return output
+
+def read_mapper_output(file, sep):
+    for line in file:
+        word, count = line.rstrip().split(sep, 1)
+        yield word, ipa2hash(word), int(count)
+
+def main(sep='\t'):
+    data = read_mapper_output(sys.stdin, sep=sep)
+    data = sorted(data, key=itemgetter(0))
+    
+    pfna_sizes = {}
+    for word, pfna, count in data:
+        if pfna not in pfna_sizes:
+            pfna_sizes[pfna] = 1
+        else:
+            pfna_sizes[pfna] += 1
+
+    for word, pfna, count in data:
+        print "%s%s%s%s%d%s%d" % (word, sep, pfna, sep, pfna_sizes[pfna], sep, count)
+
+if __name__ == "__main__":
+    CHARS = "a&@oObdDEeRfghIiklmnNprsStTUuvwjzZ"
+    CODE  = "AAAAAPPFAAAFPFAAPANNNPAFFPFAAFAAFF"    
+    CHAR_TO_CODE = maketrans(CHARS, CODE)
+    
+    main()
+    
